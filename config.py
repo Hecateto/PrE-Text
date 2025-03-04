38985b2f4228d64215600324dfd3a3a85348fad5
Hecatee <2271013961@qq.com>
2025-03-04 22:44:05 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+CACHE_DIR = './models'
+OUTPUT_DIR = './outputs'
+DATA_DIR = './data'
+DATASET_NAME = 'haiku'
+
+SIGMA = 2.31    # σ ε [2.31, 7.58] [11.3, 1.29]
+SENSITIVITY = 16    # max samples
+
+# default
+SEQ_LEN = 64
+DELTA = 3e-6
+H_MULTIPLIER = 0.25
+LOOKAHEAD = 4
+MULTIPLIER = 4
+T_STEPS = 2
+MASK = 0.3
+TRIAL = 0
